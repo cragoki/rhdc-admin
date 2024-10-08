81e3940063b7757dfdd3b4beb4bfdf9d822f239d
cragoki <55554160+cragoki@users.noreply.github.com>
2024-10-08 12:50:00 +0100

    Adding MatTable imports to create tables for API Data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { RacesComponent } from './races/races.component';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { MatInputModule } from '@angular/material/input'; 
+import { MatTableModule } from '@angular/material/table'; 
+import { MatPaginatorModule } from '@angular/material/paginator'; 
+import { MatSortModule } from '@angular/material/sort'; 
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
