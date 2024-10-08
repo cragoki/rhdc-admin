0d5ffdf8ff20af05fc479cdd04f87856ddecc547
cragoki <55554160+cragoki@users.noreply.github.com>
2024-10-08 18:48:13 +0100

    mt-table template now working

---
+++ src/app/alerts-table/alerts-table.component.spec.ts
@@ -0,0 +1,34 @@
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+
+import { AlertsTableComponent } from './alerts-table.component';
+
+describe('AlertsTableComponent', () => {
+  let component: AlertsTableComponent;
+  let fixture: ComponentFixture<AlertsTableComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [AlertsTableComponent],
+      imports: [
+        NoopAnimationsModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatTableModule,
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AlertsTableComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,22 @@ import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator'; 
 import { MatSortModule } from '@angular/material/sort'; 
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { AlertsTableComponent } from './alerts-table/alerts-table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list'; 
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav'; 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    RacesComponent
+    RacesComponent,
+    AlertsTableComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +35,14 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    LayoutModule,
+    MatButtonModule
   ],
   providers: [
     provideClientHydration()
